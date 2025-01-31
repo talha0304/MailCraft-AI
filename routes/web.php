1b85ba7a41ba6de0ce9c985366cfe4ec16e22e25
talha.saleem@code-avenue.com <talha.saleem@code-avenue.com>
2025-01-31 19:11:03 +0500

    remove extra file & use statment  and formate the html and ui code

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,13 @@
 <?php
 
-use App\Models\User;
 use Livewire\Volt\Volt;
 
 Volt::route('/', 'auth.login')->name('login');
 Volt::route('create/account', 'auth.createaccount')->name('create.account');
+Volt::route('verfication/{id}', 'emailverification')->name('email.verification');
 
 
 Route::middleware('auth')->group(function () {
-    Volt::route('verfication/{id}', 'emailverification')->name('email.verification');
     Volt::route('home', 'emailgen')->name('email.gen');
 });
 
